843be797fd757e31085c8eb9a0fabc2fe9958e6f
valeponick <vale.ponick@gmail.com>
2026-05-23 08:09:43 +0700

    "feat: add ✅ getLength() with input validation and loop"

--- PasswordTool/main.swift
+++ PasswordTool/main.swift
@@ -7,7 +7,35 @@
 
 import Foundation
 
-print("🔐   1️⃣ PASSWORD TOOL — 'генератор паролей': user вводит длину пароля. Проверка: число? в диапазоне 4–20? → если нет, ошибка. Генерация пароля (a-z, A-Z, 0-9). Вывод пароля. Цикл: 'Сгенерировать ещё? (да/нет)'")
+print("🔐 PASSWORD TOOL — 'генератор паролей'")
+print(" user вводит длину пароля. Проверка: число? в диапазоне 4–20? → если нет, ошибка. Генерация пароля (a-z, A-Z, 0-9). Вывод пароля. Цикл: 'Сгенерировать ещё? (да/нет)'")
+
+func getLength() -> Int {
+    while true { // // Цикл будет повторяться, пока не сработает return
+        // MARK: - 1️⃣ Сначала выводим подсказку в консоль
+        print("Enter length of password (4 - 20): ", terminator: "")
+        
+        // MARK: - 2️⃣ Затем считываем ввод пользователя
+        let prompt = readLine()?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // MARK: - Шаг 3️⃣ Проверка ввода
+        guard let prompt = prompt, let length = Int(prompt),
+              length >= 4 && length <= 20 else {
+            print("❌ Length is out of range. enter correct number")
+            continue
+        }
+        print("✅ Length is correct")
+        return length
+    }
+}
+
+func generatePassword(length: Int) -> String {
+    
+    
+    
+    
+    return ""
+}
 
       /* Что ты используешь: 🔐 🔤 🔢 ❌ 🔄 📝 🔥
 
@@ -20,23 +48,16 @@ print("🔐   1️⃣ PASSWORD TOOL — 'генератор паролей': use
             Спросить «Ещё? (y/n)»*/
 
 // 2️⃣ Введите длину пароля (4-20):
-
 // ❌ Ошибка: введите число от 4 до 20
-
 // 2️⃣ Введите длину пароля (4-20): 8
-
 // 5️⃣ 🔐 Ваш пароль: aK7mR2qZ
-
 // 6️⃣ Сгенерировать ещё? (y/n): n
-
 // 7️⃣ 👋 By, vale.ponick!
 
-func getLength() throws -> Int {
-    return 0
-}
+
 func generatePassword(length: Int) -> String {
     return ""
 }
 func main() {
-    return
+    
 }
